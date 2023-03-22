513c6a05f8fdf36feb37673b68721ef541f112b5
Srijani Das <srijanidas1@gmail.com>
2023-03-22 21:17:11 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+<html>
+<body>
+    <header>
+        <h1>TooDoom Drum Sound Website</h1>
+    </header>
+    <h3>Technologies Used:</h3>
+    <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+    </ul>
+    <h3>Overview:</h3>
+    <br>
+    <img src="./images/TooDoom.png">
+</body>
+</html>
---
+++ index.js
@@ -0,0 +1,62 @@
+
+var numOfDrumBtns = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numOfDrumBtns; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        var drumInnerHTML = this.innerHTML;
+        drumSound(drumInnerHTML);
+        drumAnime(drumInnerHTML)
+    });
+}
+
+document.addEventListener("keydown", function(event)
+{
+    drumSound(event.key);
+    drumAnime(event.key);
+});
+
+function drumSound(key)
+{
+    switch (key) {
+        case "w":
+            var audio = new Audio("./sounds/crash.mp3");
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio("./sounds/kick.mp3");
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio("./sounds/snare.mp3");
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio("./sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio("./sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio("./sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio("./sounds/tom-4.mp3");
+            audio.play();
+            break;
+        default:
+            console.log(key);
+            break;
+    }
+}
+
+function drumAnime(currKey) {
+    var activeDrum = document.querySelector("."+currKey);
+    activeDrum.classList.add("pressed");
+    setTimeout(function() 
+    {
+        activeDrum.classList.remove("pressed");
+    }, 100);
+}
